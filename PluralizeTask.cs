7a0856c38717af182d9780028d1a754150969d43
Dzirit <41745559+Dzirit@users.noreply.github.com>
2020-05-28 16:19:57 +0300

    fixed bugs

--- PluralizeTask.cs
+++ PluralizeTask.cs
@@ -6,9 +6,16 @@
 		{
 			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
 			if (count==1) return "рубль";
-			if (count > 19)	count %= 10;
-			if ((count == 2) || (count == 3) || (count == 4)) return "рубля";
+			var tens = count % 10;//десятки
+			if (count > 20)
+			{
+				tens = count/10%10;
+				count %= 10;
+			}
+			if (count == 1 && tens != 1) return "рубль";
+			if (tens != 1 && ((count == 2) || (count == 3) || (count == 4)) ) return "рубля";
 			else  return "рублей";
+			//else if (count == 1) return "рубль";
 		}
 	}
 }
