98e149c4dc6301fd58f2d88399025809347bec4d
Dzirit <41745559+Dzirit@users.noreply.github.com>
2020-05-28 16:01:34 +0300

    Добавьте файлы проекта.

---
+++ PluralizeTask.cs
@@ -0,0 +1,14 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+			if (count==1) return "рубль";
+			if (count > 19)	count %= 10;
+			if ((count == 2) || (count == 3) || (count == 4)) return "рубля";
+			else  return "рублей";
+		}
+	}
+}
